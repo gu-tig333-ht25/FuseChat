71995ad53b55973dcf688fe17a7362b8e178d051
Viktor Gunnarsson <viktor.gunnarsson@gmail.com>
2025-10-13 11:57:40 +0200

    Add chat screen, theme edits

--- lib/theme/themedata.dart
+++ lib/theme/themedata.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData fuseChatDarkTheme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.dark,
-  scaffoldBackgroundColor: const Color(0xFF121212), 
+  scaffoldBackgroundColor: const Color(0xFF121212),
   colorScheme: const ColorScheme.dark(
-    primary: Colors.grey,        
-    secondary: Color(0xFF2D5D00),      
-    surface: Color(0xFF1E1E1E),        
-    onSurface: Colors.grey,          
+    primary: Colors.grey,
+    secondary: Color(0xFF2D5D00),
+    surface: Color(0xFF1E1E1E),
+    onSurface: Colors.grey,
   ),
-  appBarTheme: const AppBarTheme(
+  appBarTheme: AppBarTheme(
     backgroundColor: Colors.transparent,
     surfaceTintColor: Colors.transparent,
     elevation: 0,
     foregroundColor: Colors.white,
     centerTitle: true,
+    titleTextStyle: GoogleFonts.instrumentSans(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      letterSpacing: 1.5,
+      color: Colors.white,
+    ),
   ),
-  cardColor:  Color(0xFF1E1E1E),
+  cardColor: Color(0xFF1E1E1E),
   searchBarTheme: const SearchBarThemeData(
     backgroundColor: WidgetStatePropertyAll(Colors.white),
-    textStyle:  WidgetStatePropertyAll(TextStyle(color: Colors.black)), 
-    hintStyle: WidgetStatePropertyAll(
-      TextStyle(color: Colors.black54),
-    ),
+    textStyle: WidgetStatePropertyAll(TextStyle(color: Colors.black)),
+    hintStyle: WidgetStatePropertyAll(TextStyle(color: Colors.black54)),
   ),
   inputDecorationTheme: InputDecorationTheme(
     filled: true,
-    fillColor:Color(0xFF1E1E1E),
+    fillColor: Color(0xFF1E1E1E),
     border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(12),
+      borderRadius: BorderRadius.circular(30.0),
+      borderSide: BorderSide.none,
     ),
     hintStyle: const TextStyle(color: Colors.grey),
   ),
